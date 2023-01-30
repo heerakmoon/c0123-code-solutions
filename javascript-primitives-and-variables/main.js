fd944c3be3ace96477b5b98bf8b881dcea85eb08
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-30 17:28:21 +0000

    Logged typeof for each variable to the console with labels

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,7 @@
+var fullName = 'Heerak Moon';
+var isCool = false;
+var totalPets = 4;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
