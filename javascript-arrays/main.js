5542c9e96b39c4891416c3f92e2b0e28456a2ec5
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-30 23:03:28 +0000

    Completed javascript-arrays exercise

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -9,3 +9,14 @@ colors[2] = 'green';
 
 console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
 console.log('value of colors:', colors);
+
+var students = ['Leonardo', 'Donatello', 'Raphael', 'Michelangelo'];
+var numberOfStudents = students.length;
+
+console.log('There are ' + numberOfStudents + ' students in the class.');
+
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
+
+console.log('The last student in the array is ' + lastStudent + '.');
+console.log('value of students:', students);
