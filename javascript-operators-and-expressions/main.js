6cac3a5d9293354b9d48cd9ef1e119a3b543a284
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-30 19:35:12 +0000

    Declared pH/isAcidic variables and logged isAcidic value and type of to the console

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -26,3 +26,9 @@ var fullName = firstName + ' ' + lastName;
 
 console.log('value of fullName:', fullName);
 console.log('typeof fullName:', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', isAcidic);
