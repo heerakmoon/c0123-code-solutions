46f52032d316cb1932fff1464d55cca5dccb58e7
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-31 23:41:14 +0000

    Completed array methods

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -17,3 +17,40 @@ console.log('value of randomIndex:', randomIndex);
 var randomHero = heroes[randomIndex];
 
 console.log('value of randomHero:', randomHero);
+
+var library = [
+  {
+    title: 'Of Mice and Men',
+    author: 'John Steinbeck'
+  },
+  {
+    title: 'The Call of the Wild',
+    author: 'Jack London'
+  },
+  {
+    title: 'Black Beauty',
+    author: 'Anna Sewell'
+  }
+];
+var lastBook = library.pop();
+
+console.log('value of lastBook:', lastBook);
+
+var firstBook = library.shift();
+
+console.log('value of firstBook:', firstBook);
+
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+
+library.push(js);
+library.unshift(css);
+library.splice(1, 1);
+
+console.log('value of library:', library);
