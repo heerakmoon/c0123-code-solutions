34d9b51d48175b2caedf097a4f33d5f2a34ca882
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-01 19:39:10 +0000

    created getNumbersToten function

---
+++ javascript-loops/main.js
@@ -0,0 +1,20 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+
+function getNumbersToTen() {
+  var numbers = [];
+  var currentNumber = 1;
+  while (currentNumber <= 10) {
+    numbers.push(currentNumber);
+    currentNumber++;
+  }
+  return numbers;
+}
+
+console.log('getNumbersToTen()', getNumbersToTen(15));
