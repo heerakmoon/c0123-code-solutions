67d9149728b01e803441752bdef18cc2aed88ff7
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-02 00:07:05 +0000

    filterOutNulls(values) function created

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -1,1 +1,11 @@
 /* exported filterOutNulls */
+
+function filterOutNulls(values) {
+  var noNullsArr = [];
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      noNullsArr.push(values[i]);
+    }
+  }
+  return noNullsArr;
+}
