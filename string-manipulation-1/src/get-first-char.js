8da3cb8b04d148fe315400c54b8fb32b45149771
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-02 23:34:13 +0000

    Pseudocode and function for getFirstChar(string)

---
+++ string-manipulation-1/src/get-first-char.js
@@ -0,0 +1,9 @@
+/* exported getFirstChar */
+/*
+* Define a function, named getFirstChar, with one parameter, string
+*   return the string parameter at the first index, 0
+ */
+
+function getFirstChar(string) {
+  return string[0];
+}
