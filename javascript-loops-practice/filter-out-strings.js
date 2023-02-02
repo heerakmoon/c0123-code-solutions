3b68237f51b3e068d1fc11062679422a9a517438
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-02 00:21:49 +0000

    filterOutStrings(values) function created

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -1,1 +1,11 @@
 /* exported filterOutStrings */
+
+function filterOutStrings(values) {
+  var noStringsArr = [];
+  for (var i = 0; i < values.length; i++) {
+    if (typeof values[i] !== 'string') {
+      noStringsArr.push(values[i]);
+    }
+  }
+  return noStringsArr;
+}
