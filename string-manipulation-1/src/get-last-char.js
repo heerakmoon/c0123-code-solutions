76ac1ee6a0b474299768aa1170939ad73030e68d
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-02 23:40:21 +0000

    Pseudocode and function for getLastChar

--- string-manipulation-1/src/get-last-char.js
+++ string-manipulation-1/src/get-last-char.js
@@ -1,1 +1,8 @@
 /* exported getLastChar */
+/*
+* Define a function, named getLastChar, with one parameter, string
+*   return the character at the last index of the string parameter */
+
+function getLastChar(string) {
+  return string[string.length - 1];
+}
