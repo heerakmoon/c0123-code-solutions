b0f74d7583b236c822e77194ea7f0ac75e996fe5
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-03 03:18:14 +0000

    Pseudocode and function for initial(array)

--- array-manipulation-1/src/initial.js
+++ array-manipulation-1/src/initial.js
@@ -1,1 +1,17 @@
 /* exported initial */
+/*
+* Define a function, named initial, with one parameter, array
+*   Create a new variable, noLastElement, with the value of an empty array
+*   Use a loop to iterate through array, but only iterate up to the second to last item
+*   At each index,
+*     Add the item at that index to the noLastElement array
+*   Return the noLastElement variable out of the function
+*/
+
+function initial(array) {
+  var noLastElement = [];
+  for (var i = 0; i < array.length - 1; i++) {
+    noLastElement.push(array[i]);
+  }
+  return noLastElement;
+}
