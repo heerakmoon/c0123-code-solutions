f9dc216656ac7c8d2562158304a46120a6892727
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-03 02:13:11 +0000

    Pseudocode and function for reverseWord. String-manipulation-1 exercise complete

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,16 @@
 /* exported reverseWord */
+/*
+* Define a function, named reverseWord, with one parameter, word
+*   Create a new variable, wordReversed, with a value of an empty string
+*   Use a loop to iterate through word string, but start at the last index and decrement
+*     during each iteration, concatenate the character at the index to the wordReversed variable
+*   Return the wordReversed variable out of the function
+*/
+
+function reverseWord(word) {
+  var wordReversed = '';
+  for (var i = word.length - 1; i >= 0; i--) {
+    wordReversed += word[i];
+  }
+  return wordReversed;
+}
