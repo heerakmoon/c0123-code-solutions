2487c6ae35e17da3ba3171affbf8dda4e91b7b34
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-03 06:09:03 +0000

    pseudocode and function for getValue(object, key)

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,9 @@
+/* exported getValue */
+/*
+* Define a function, named getValue, with two parameters, object and key
+*   Return the value of key in the object
+*/
+
+function getValue(object, key) {
+  return object[key];
+}
