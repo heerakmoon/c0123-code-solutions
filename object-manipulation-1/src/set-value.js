bd493970abe75691ce63f04fc6e5a1fb7aa5f52b
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-03 06:17:37 +0000

    Pseudocode and function for setValue(object, key, value)

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,1 +1,9 @@
 /* exported setValue */
+/*
+* Define a function, setValue, with three parameters, object, key, and value
+*   Assign value to the value of key from object
+*/
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
