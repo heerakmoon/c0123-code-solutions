2c5d72d32ce63c118aef9ff56673ad472ab6999b
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-03 04:01:42 +0000

    Pseudocode and function for compact(array). Completed array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,19 @@
 /* exported compact */
+/*
+* Define a function, named compact, with one parameter, array
+*   Create a new variable, noFalsys, with the value of an empty array
+*   Use a loop to iterate through array, at each index
+*     Check if the item at the index is true(truthy)
+*       If the item is true, then push the item into the noFalsys array
+*   Return the noFalsys variable out of the function
+*/
+
+function compact(array) {
+  var noFalsys = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      noFalsys.push(array[i]);
+    }
+  }
+  return noFalsys;
+}
