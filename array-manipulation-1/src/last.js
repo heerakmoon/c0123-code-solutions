4214ad76ec594adb5ca2a34451e55d21bd295c36
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-03 02:50:49 +0000

    Pseudocode and function for last(array)

--- array-manipulation-1/src/last.js
+++ array-manipulation-1/src/last.js
@@ -1,1 +1,9 @@
 /* exported last */
+/*
+* Define a function, named last, with one parameter, array
+*    Return the last item of the array out of the function
+*/
+
+function last(array) {
+  return array[array.length - 1];
+}
