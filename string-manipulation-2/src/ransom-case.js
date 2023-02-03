dbae7da1164c668e38af9562fe11967be900b2e3
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-03 18:31:46 +0000

    Pseudocode and function for ransomCase(string)

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,23 @@
 /* exported ransomCase */
+/*
+* Define a function, named ransomCase, with one parameter, string
+*   Create a variable, everyOther, with the value of an empty string
+*   Use a loop to iterate through string, for each iteration
+*     Check if the index is odd or even, if it's even
+*       Lowercase the character at the index and add it to the everyOther variable
+*     If the index is odd
+*       Uppercase the character at the index and add it to the everyOther variable
+*   Return the everyOther variable
+*/
+
+function ransomCase(string) {
+  var everyOther = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      everyOther += string[i].toLowerCase();
+    } else {
+      everyOther += string[i].toUpperCase();
+    }
+  }
+  return everyOther;
+}
