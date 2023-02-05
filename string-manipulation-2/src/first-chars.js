d1d222187b8a8e7ac474377af47bdd95ed9b36c4
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-05 20:56:29 +0000

    Pseudocode and function for firstChars(length, string)

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,19 @@
 /* exported firstChars */
+/*
+* Define a function, named firstChars, with two parameters, length and string
+*   Create a variable, firstLetters, with a value of an empty string
+*   Use a loop to iterate through string, for each iteration
+*     Check if the index is less than the length parameter, if it is
+*       Add the character at the index to the firstLetters variable
+*   Return the firstLetters variable out of the function
+*/
+
+function firstChars(length, string) {
+  var firstLetters = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i < length) {
+      firstLetters += string[i];
+    }
+  }
+  return firstLetters;
+}
