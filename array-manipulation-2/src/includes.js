1ecd9b1b996e18626e44318c2e20b4a72adcd23e
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-06 03:25:27 +0000

    Pseudocode and function for includes(array, value)

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,18 @@
 /* exported includes */
+/*
+* Define a function, named includes, with two parameters, array and value
+*   Use a loop to iterate through array, with each iteration
+*     Check if the value at the index is the same as value, if it is
+*       Return true
+*   If none of the items in the array are the same as value
+*   Return false
+*/
+
+function includes(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
