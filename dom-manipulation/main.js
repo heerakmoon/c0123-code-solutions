5fed14909abfc8f088f4f2cf2232fb8d19900e48
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-06 23:03:16 +0000

    Declared three new variables. Added a click event listener. In the anonymous function, numberOfClicks variable is incremented

---
+++ dom-manipulation/main.js
@@ -0,0 +1,7 @@
+// var numberOfClicks = 0;
+var $hotButton = document.querySelector('.hot-button');
+// var $clickCount = document.querySelector('.click-count');
+
+$hotButton.addEventListener('click', function (event) {
+  // numberOfClicks++;
+});
