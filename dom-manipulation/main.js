f46abd3f24293318870ec3ef48f5ca42b72233ce
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-06 23:25:21 +0000

    Update p element text and button's class according to number of clicks

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,7 +1,21 @@
-// var numberOfClicks = 0;
+var numberOfClicks = 0;
 var $hotButton = document.querySelector('.hot-button');
-// var $clickCount = document.querySelector('.click-count');
+var $clickCount = document.querySelector('.click-count');
 
 $hotButton.addEventListener('click', function (event) {
-  // numberOfClicks++;
+  numberOfClicks++;
+  $clickCount.textContent = 'Clicks: ' + numberOfClicks;
+  if (numberOfClicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (numberOfClicks < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (numberOfClicks < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (numberOfClicks < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (numberOfClicks < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
 });
