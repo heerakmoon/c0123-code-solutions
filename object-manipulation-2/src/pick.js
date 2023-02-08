f4f54a6e3e2f7081f69bd72383caaa55f88a57f3
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-08 00:53:04 +0000

    pick function in progress

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,17 @@
+/* exported pick */
+/*
+* Define a function, named pick, with two parameters, source and keys
+*   Create a variable, pickObj, with the value of an empty object literal
+*   Use a loop to iterate through source's properties, with each iteration
+*     Check if the property is the same as one of the keys, if there is a match
+*       Add the property and its value to the pickObj object
+*   Return the pickObj variable
+*/
+
+function pick(source, keys) {
+  var pickObj = {};
+  // for (var key in source) {
+  //   i
+  // }
+  return pickObj;
+}
