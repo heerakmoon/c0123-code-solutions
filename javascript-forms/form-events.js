b25a20d4e8d127063f35bb405556aec47c2335b7
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-08 17:53:41 +0000

    handleFocus, handleBlur, and handleInput functions

---
+++ javascript-forms/form-events.js
@@ -0,0 +1,13 @@
+// function handleFocus(e) {
+//   console.log('focus event fired');
+//   console.log('event.target.name:', e.target.name);
+// }
+
+// function handleBlur(e) {
+//   console.log('blur event fired');
+//   console.log('event.target.name:', e.target.name);
+// }
+
+// function handleInput(e) {
+//   console.log('event.target.name:', e.target.name, 'event.target.value:', e.target.value);
+// }
