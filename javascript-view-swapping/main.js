f78e3714b439e2520d99a6c956f0afb0339e189f
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-09 21:56:31 +0000

    Added click event listener to element with class tab-container and an anonymous function

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,3 +1,15 @@
-// var $tabContainer = document.querySelector('.tab-container');
-// var $tabs = document.querySelectorAll('.tab');
+var $tabContainer = document.querySelector('.tab-container');
+var $tabs = document.querySelectorAll('.tab');
 // var $views = document.querySelectorAll('.view');
+
+$tabContainer.addEventListener('click', function (e) {
+  if (e.target.matches('.tab')) {
+    for (var i = 0; i < $tabs.length; i++) {
+      if ($tabs[i] === e.target) {
+        $tabs[i].className += ' active';
+      } else {
+        $tabs[i].className = 'tab';
+      }
+    }
+  }
+});
