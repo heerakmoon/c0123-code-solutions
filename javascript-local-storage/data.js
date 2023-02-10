29b6c38ce7434ce7dcd401b44cb8d4437e927f77
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-10 18:57:08 +0000

    Completed javascript-local-storage exercise

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,6 +1,11 @@
 /* exported todos */
 
 var todos = [];
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
 
 window.addEventListener('beforeunload', function (e) {
   var todosJSON = JSON.stringify(todos);
