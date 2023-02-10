e67b943a06739c1e19c562a9dc840973729a19ea
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-10 17:25:16 +0000

    omit function in progress

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,13 @@
 /* exported omit */
+/*
+* Define a function, named omit, with two parameters, source and keys
+*   Create a variable, omitObj, with the value of an empty object
+*
+*/
+
+// function omit(source, keys) {
+//   var omitObj = {};
+//   for (var key in source) {
+//   }
+//   return omitObj;
+// }
