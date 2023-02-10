ff667d58f564d3d9083ff1f7d607fbf3ab32064c
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-10 17:52:10 +0000

    Created json string of books array

--- javascript-and-json/main.js
+++ javascript-and-json/main.js
@@ -1,17 +1,25 @@
-// var books = [
-//   {
-//     title: 'Pride and Prejudice',
-//     author: 'Jane Austen',
-//     isbn: '0000'
-//   },
-//   {
-//     title: 'The Adventures of Tom Sawyer',
-//     author: 'Mark Twain',
-//     isbn: '0123'
-//   },
-//   {
-//     title: 'Frankenstein',
-//     author: 'Mary Shelley',
-//     isbn: '0101'
-//   }
-// ];
+var books = [
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    isbn: '0000'
+  },
+  {
+    title: 'The Adventures of Tom Sawyer',
+    author: 'Mark Twain',
+    isbn: '0123'
+  },
+  {
+    title: 'Frankenstein',
+    author: 'Mary Shelley',
+    isbn: '0101'
+  }
+];
+
+console.log('books array:', books);
+console.log('typeof books:', typeof books);
+
+var booksStringify = JSON.stringify(books);
+
+console.log('booksStringify array:', booksStringify);
+console.log('typeof booksStringify:', typeof booksStringify);
