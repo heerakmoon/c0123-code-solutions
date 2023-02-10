fbac82abde883c39f923caf3b03d146eead91cac
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-10 17:41:44 +0000

    Created array with three obj literals that represent books

---
+++ javascript-and-json/main.js
@@ -0,0 +1,17 @@
+// var books = [
+//   {
+//     title: 'Pride and Prejudice',
+//     author: 'Jane Austen',
+//     isbn: '0000'
+//   },
+//   {
+//     title: 'The Adventures of Tom Sawyer',
+//     author: 'Mark Twain',
+//     isbn: '0123'
+//   },
+//   {
+//     title: 'Frankenstein',
+//     author: 'Mary Shelley',
+//     isbn: '0101'
+//   }
+// ];
