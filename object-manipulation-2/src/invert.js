c1d72cc2cb24fd455d116df4ec9e354a06831ad4
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-11 02:22:43 +0000

    Pseudocode and function for invert(source)

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,16 @@
 /* exported invert */
+/*
+* Define a function, named invert, with one parameter, source
+*   Create a variable, invertedObj, with the value of an empty object literal
+*   Use a loop to iterate through the source object's properties, with each iteration
+*     Assign the value as the invertedObj's property and the property as the value
+*   Return the invertedObj variable
+*/
+
+function invert(source) {
+  var invertedObj = {};
+  for (var prop in source) {
+    invertedObj[source[prop]] = prop;
+  }
+  return invertedObj;
+}
