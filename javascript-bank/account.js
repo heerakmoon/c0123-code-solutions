b93b19f493cf0611a40d3752d76d776e6514efb6
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-21 21:09:12 +0000

    created a deposit prototype method of the account constructor

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,1 +1,17 @@
 /* exported Account */
+
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (amount > 0) {
+    var newDeposit = new Transaction('deposit', amount);
+    this.transactions.push(newDeposit);
+    return true;
+  } else {
+    return false;
+  }
+};
