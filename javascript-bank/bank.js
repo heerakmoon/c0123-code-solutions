f8f577dc22bd1fe0ffe83cf29a32b87146d4ac03
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-22 00:34:29 +0000

    created a getTotalAssets prototype method of the Bank constructor that calculates the grand total of all balances of all accounts in the bank. Completed javascript-bank challenge

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -25,3 +25,12 @@ Bank.prototype.getAccount = function (number) {
   }
   return null;
 };
+
+Bank.prototype.getTotalAssets = function () {
+  var grandTotal = 0;
+  for (var i = 0; i < this.accounts.length; i++) {
+    var accountBalance = this.accounts[i].getBalance();
+    grandTotal += accountBalance;
+  }
+  return grandTotal;
+};
