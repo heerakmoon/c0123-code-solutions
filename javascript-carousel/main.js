712df4aafbdf0a700f5719cdf85286f88bdf6d1b
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-22 22:51:50 +0000

    Added ids to left and right caret icons. Created variables for dom elements

---
+++ javascript-carousel/main.js
@@ -0,0 +1,6 @@
+// var $leftArrow = document.querySelector('#left');
+// var $rightArrow = document.querySelector('#right');
+// var $circles = document.querySelectorAll('.fa-circle');
+// var $images = document.querySelectorAll('img');
+
+// var indexCounter = 0;
