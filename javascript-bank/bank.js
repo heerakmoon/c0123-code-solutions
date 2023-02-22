d32b84fc0a902677baf6a612f782079fd8ec6733
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-22 00:10:13 +0000

    Created a openAccount prototype method of the Bank constructor

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,18 @@
 /* exported Bank */
+
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (Number.isInteger(balance) && balance > 0) {
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  } else {
+    return null;
+  }
+};
