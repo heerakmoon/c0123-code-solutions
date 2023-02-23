a341d0d4f25f915c5875472c608cdd3f4cbcb19f
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-23 00:25:01 +0000

    Added click event listener to right arrow to swap images and change circles

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,6 +1,14 @@
 // var $leftArrow = document.querySelector('#left');
-// var $rightArrow = document.querySelector('#right');
-// var $circles = document.querySelectorAll('.fa-circle');
-// var $images = document.querySelectorAll('img');
+var $rightArrow = document.querySelector('#right');
+var $circles = document.querySelectorAll('.fa-circle');
+var $images = document.querySelectorAll('img');
 
-// var indexCounter = 0;
+var indexCounter = 0;
+
+$rightArrow.addEventListener('click', function (event) {
+  $images[indexCounter].className = 'hidden';
+  $circles[indexCounter].className = 'far fa-circle';
+  ++indexCounter;
+  $images[indexCounter].className = '';
+  $circles[indexCounter].className = 'fas fa-circle';
+});
