e819568e8e6d4f01c76ecb66b8c770a53477b21a
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-23 01:19:24 +0000

    Created rulesets and a function that changes the background color according to which pokemon shows. Edited event handler for next and previous arrows

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -2,21 +2,45 @@ var $leftArrow = document.querySelector('#left');
 var $rightArrow = document.querySelector('#right');
 var $circles = document.querySelectorAll('.fa-circle');
 var $images = document.querySelectorAll('img');
+var $body = document.querySelector('body');
 
 var indexCounter = 0;
+$body.className = 'bulbasaur';
 
 $rightArrow.addEventListener('click', function (event) {
-  $images[indexCounter].className = 'hidden';
-  $circles[indexCounter].className = 'far fa-circle';
+  hideImg();
   ++indexCounter;
-  $images[indexCounter].className = '';
-  $circles[indexCounter].className = 'fas fa-circle';
+  showImg();
+  changeBackgroundColor();
 });
 
 $leftArrow.addEventListener('click', function (event) {
+  hideImg();
+  --indexCounter;
+  showImg();
+  changeBackgroundColor();
+});
+
+function hideImg() {
   $images[indexCounter].className = 'hidden';
   $circles[indexCounter].className = 'far fa-circle';
-  --indexCounter;
+}
+
+function showImg() {
   $images[indexCounter].className = '';
   $circles[indexCounter].className = 'fas fa-circle';
-});
+}
+
+function changeBackgroundColor() {
+  if (indexCounter === 0) {
+    $body.className = 'bulbasaur';
+  } else if (indexCounter === 1) {
+    $body.className = 'charmander';
+  } else if (indexCounter === 2) {
+    $body.className = 'squirtle';
+  } else if (indexCounter === 3) {
+    $body.className = 'pikachu';
+  } else if (indexCounter === 4) {
+    $body.className = 'jiggly-puff';
+  }
+}
