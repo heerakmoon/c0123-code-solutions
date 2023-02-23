f9bc09edc0d09f3c58ed4acfa0c60c6aa1bc9c52
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-23 00:31:25 +0000

    Added click event listener to left arrow that switches to previous image and bubble

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,4 +1,4 @@
-// var $leftArrow = document.querySelector('#left');
+var $leftArrow = document.querySelector('#left');
 var $rightArrow = document.querySelector('#right');
 var $circles = document.querySelectorAll('.fa-circle');
 var $images = document.querySelectorAll('img');
@@ -9,6 +9,14 @@ $rightArrow.addEventListener('click', function (event) {
   $images[indexCounter].className = 'hidden';
   $circles[indexCounter].className = 'far fa-circle';
   ++indexCounter;
+  $images[indexCounter].className = '';
+  $circles[indexCounter].className = 'fas fa-circle';
+});
+
+$leftArrow.addEventListener('click', function (event) {
+  $images[indexCounter].className = 'hidden';
+  $circles[indexCounter].className = 'far fa-circle';
+  --indexCounter;
   $images[indexCounter].className = '';
   $circles[indexCounter].className = 'fas fa-circle';
 });
