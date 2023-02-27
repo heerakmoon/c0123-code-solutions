ba034f447d4f9d7900307116a981833fbce2afc9
Heerak Moon <hmoon3@student.mtsac.edu>
2023-02-27 09:46:03 +0000

    isAnagram function in progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,30 @@
 /* exported isAnagram */
+/*
+* Define a function, named letters, that counts the number of characters in a string, excluding spaces
+* Define a function, named isAnagram, with two parameters, firstString and secondString
+*   Create a variable, firstStringChars, with the value of the return value from the letters function with an argument of firstString
+*   Create a variable, secondStringChars, with the value of the return value from the letters function with an argument of secondString
+*   Check if the number of firstStringChars and secondStringChars are the same, if they're not
+*     return false
+*   If they are the same,
+*
+*/
+
+function letters(string) {
+  var numOfLetters = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      numOfLetters++;
+    }
+  }
+  return numOfLetters;
+}
+
+function isAnagram(firstString, secondString) {
+  var firstStringChars = letters(firstString);
+  var secondStringChars = letters(secondString);
+  if (firstStringChars !== secondStringChars) {
+    return false;
+  // } else {
+  }
+}
