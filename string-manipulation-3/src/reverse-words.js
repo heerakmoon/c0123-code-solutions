3d2049a1f61d75686494265fd3512558f121db8b
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-05 00:49:23 +0000

    reverseWords pseudocode and function created

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,33 +1,40 @@
 /* exported reverseWords */
 /*
 * Define a function, named reverseWords, with one parameter, string
-*   Create a variable, named stringArr, with a value of an array with each item being the string separated at each space
-*   Create a variable, named reverseString, with a value of an empty string
+*   Create a variable, named stringArr, with a value of an array with each item being a word from string
+*   Create a variable, named reversedWordsArr, with a value of an empty array
 *     Loop through the stringArr, with each iteration
-*       Place the item into a variable, item
 *       Create a variable, backward, with a value of an empty string
-*       Loop through the characters of each item, starting at the last character
+*       Loop through the characters of each item from StringArr, starting at the last character
 *         Concatenate each character to the backward variable
-*       Concatenate the backward variable to the reverseString variable
-*       Check if the value of each index is the last item, if it's not
-*         Concatenate a space to the reverseString variable
+*   Create a variable, named reverseString, with a value of an empty string
+*   Loop through the reversedWordsArr, with each iteration
+*     Concantenate the word to the reverseString variable
+*     Check if the word is the last word in the array, if it's not
+*       Concatenate a space
 *   Return the reverseString variable
 */
 
 function reverseWords(string) {
   var stringArr = string.split(' ');
-  var reverseString = '';
+  var reversedWordsArr = [];
 
   for (var m = 0; m < stringArr.length; m++) {
-    var item = stringArr[m];
     var backward = '';
-    for (var i = item.length - 1; i >= 0; i--) {
-      backward += item[i];
+    for (var i = stringArr[m].length - 1; i >= 0; i--) {
+      backward += stringArr[m][i];
     }
-    reverseString += backward;
-    if (item[m] !== item[stringArr.length - 1]) {
+    reversedWordsArr.push(backward);
+  }
+
+  var reverseString = '';
+
+  for (var k = 0; k < reversedWordsArr.length; k++) {
+    reverseString += reversedWordsArr[k];
+    if (k !== reversedWordsArr.length - 1) {
       reverseString += ' ';
     }
   }
+
   return reverseString;
 }
