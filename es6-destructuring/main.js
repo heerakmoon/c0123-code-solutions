33bf365f19dd80fc16f1ac79f052058f4690ed59
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-13 21:22:40 +0000

    Destructured library array and logged book items in library to console

--- es6-destructuring/main.js
+++ es6-destructuring/main.js
@@ -21,3 +21,36 @@ const { author: book2Author } = book2;
 const { libraryID: book2ID } = book2;
 
 console.log('The title of the book is ' + book2Title + ', the author is ' + book2Author + ', and the library id is ' + book2ID);
+
+const library = [
+  {
+    title: 'The Road Ahead',
+    author: 'Bill Gates',
+    libraryID: 1254
+  },
+  {
+    title: 'Walter Isaacson',
+    author: 'Steve Jobs',
+    libraryID: 4264
+  },
+  {
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    author: 'Suzanne Collins',
+    libraryID: 3245
+  },
+  {
+    title: "Dead Dead Demon's De De De De Destruction",
+    author: 'Inio Asano',
+    libraryID: 1233
+  }
+];
+
+const [book3, book4, book5] = library;
+
+console.log('book3:', book3);
+console.log('book4:', book4);
+console.log('book5:', book5);
+
+const [, book6] = library;
+
+console.log('book6:', book6);
