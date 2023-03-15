9b6d9fffd2d41c117d23c17b42cc873ec30cace4
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-15 16:05:53 +0000

    created variables for process.argv

--- node-module-system/calculate.js
+++ node-module-system/calculate.js
@@ -3,12 +3,16 @@ import subtract from './subtract.js';
 import multiply from './multiply.js';
 import divide from './divide.js';
 
-if (process.argv[3] === 'plus') {
-  console.log('result:', add(Number(process.argv[2]), Number(process.argv[4])));
-} else if (process.argv[3] === 'minus') {
-  console.log('result:', subtract(Number(process.argv[2]), Number(process.argv[4])));
-} else if (process.argv[3] === 'over') {
-  console.log('result:', divide(Number(process.argv[2]), Number(process.argv[4])));
-} else if (process.argv[3] === 'times') {
-  console.log('result:', multiply(Number(process.argv[2]), Number(process.argv[4])));
+const thirdArg = Number(process.argv[2]);
+const fourthArg = process.argv[3];
+const fifthArg = Number(process.argv[4]);
+
+if (fourthArg === 'plus') {
+  console.log('result:', add(thirdArg, fifthArg));
+} else if (fourthArg === 'minus') {
+  console.log('result:', subtract(thirdArg, fifthArg));
+} else if (fourthArg === 'over') {
+  console.log('result:', divide(thirdArg, fifthArg));
+} else if (fourthArg === 'times') {
+  console.log('result:', multiply(thirdArg, fifthArg));
 }
