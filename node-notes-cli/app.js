5352b83ab4b130cd650f9ecc3924931fbaa41670
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-17 22:43:14 +0000

    Created add feature, allows user to append new note to list of notes

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,6 +1,7 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 
 const feature = process.argv[2];
+const newNote = process.argv[3] + '';
 
 if (feature === 'read') {
   readFile('data.json', 'utf-8')
@@ -12,4 +13,16 @@ if (feature === 'read') {
       }
     })
     .catch((err) => console.log(err.message));
+
+} else if (feature === 'create') {
+  readFile('data.json', 'utf-8')
+    .then((data) => {
+      const dataParsed = JSON.parse(data);
+      const notes = dataParsed.notes;
+      const nextId = dataParsed.nextId;
+      notes[nextId] = newNote;
+      dataParsed.nextId = nextId + 1;
+      writeFile('data.json', JSON.stringify(dataParsed, null, 2), 'utf-8');
+    })
+    .catch((err) => console.error(err));
 }
