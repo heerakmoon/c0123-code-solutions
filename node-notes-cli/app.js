bf1d5621ce7a61bec31db69ad60a14fa77f81b07
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-20 05:20:50 +0000

    First two features edited into one then

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -3,26 +3,19 @@ import { readFile, writeFile } from 'node:fs/promises';
 const feature = process.argv[2];
 const newNote = process.argv[3] + '';
 
-if (feature === 'read') {
-  readFile('data.json', 'utf-8')
-    .then((data) => {
-      const dataParsed = JSON.parse(data);
-      const notes = dataParsed.notes;
+readFile('data.json', 'utf-8')
+  .then((data) => {
+    const dataParsed = JSON.parse(data);
+    const notes = dataParsed.notes;
+    const nextId = dataParsed.nextId;
+    if (feature === 'read') {
       for (const id in notes) {
         console.log(`${id}: ${notes[id]}`);
       }
-    })
-    .catch((err) => console.log(err.message));
-
-} else if (feature === 'create') {
-  readFile('data.json', 'utf-8')
-    .then((data) => {
-      const dataParsed = JSON.parse(data);
-      const notes = dataParsed.notes;
-      const nextId = dataParsed.nextId;
+    } else if (feature === 'create') {
       notes[nextId] = newNote;
       dataParsed.nextId = nextId + 1;
       writeFile('data.json', JSON.stringify(dataParsed, null, 2), 'utf-8');
-    })
-    .catch((err) => console.error(err));
-}
+    }
+  })
+  .catch((err) => console.error(err.message));
