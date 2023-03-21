e484f410f50dfc25fb7909f5e9de022979805831
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-21 21:16:26 +0000

    express-post-json post request in progress

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -4,12 +4,19 @@ const app = express();
 // const nextId = 1;
 const grades = {};
 
+app.use(express.json());
+
 app.get('/api/grades', (req, res) => {
   const gradesArr = [];
   for (const id in grades) {
     gradesArr.push(grades[id]);
   }
   res.json(gradesArr);
+});
+
+app.post('/api/grades', (req, res) => {
+  // const student = req.body;
+  res.send(201);
 });
 
 app.listen(8080, () => {
