4afb57e1b51cbd58cc3f33b2ab3107b630243cdc
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-22 17:08:27 +0000

    Clients can get a single note by id: in progress

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -2,12 +2,38 @@ import express from 'express';
 const port = 8080;
 
 const app = express();
-const notes = [];
+const notes = [
+  {
+    content: 'hi',
+    id: 2
+  }
+];
+const badReq = {
+  error: 'id must be positive integer'
+};
 
 app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
   res.status(200).json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  console.log(req.params.id);
+  if (req.params.id < 0) {
+    res.status(400).json(badReq);
+  } else if (req.params.id) {
+    let targetId;
+    for (const note in notes) {
+      if (note.id === req.params.id) {
+        targetId = note.id;
+        req.status(200).json(targetId);
+        console.log(targetId);
+      } else {
+        // req.status(404).json()
+      }
+    }
+  }
 });
 
 app.listen(port, () => console.log(`Express server listening on port ${port}`));
