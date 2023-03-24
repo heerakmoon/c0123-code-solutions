a384254f2d1878c2ee445ab47a78e26a818e74da
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-24 18:30:49 +0000

    readFile and json parse to get obj from data.json

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
+import { readFileSync } from 'node:fs';
 const port = 8080;
 
+const readData = () => {
+  const dataJSON = JSON.parse(readFileSync('data.json', 'utf-8'));
+  return dataJSON;
+};
+const notes = [];
 const app = express();
-const notes = [
-  {
-    content: 'hi',
-    id: 2
-  }
-];
+
 const badReq = {
   error: 'id must be positive integer'
 };
@@ -15,6 +16,10 @@ const badReq = {
 app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
+  const notesObj = readData();
+  for (const note in notesObj.notes) {
+    notes.push(notesObj.notes[note]);
+  }
   res.status(200).json(notes);
 });
 
@@ -22,10 +27,10 @@ app.get('/api/notes/:id', (req, res) => {
   console.log(req.params.id);
   if (req.params.id < 0) {
     res.status(400).json(badReq);
-  } else if (req.params.id) {
+  } else if (req.params.id > 0) {
     let targetId;
     for (const note in notes) {
-      if (note.id === req.params.id) {
+      if (note === req.params.id) {
         targetId = note.id;
         req.status(200).json(targetId);
         console.log(targetId);
