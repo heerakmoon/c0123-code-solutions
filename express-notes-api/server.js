6ac95009346acbf11c861d5c4ee308bba2b06610
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-24 22:23:16 +0000

    GET a single note by id: in progress

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -6,34 +6,32 @@ const readData = () => {
   const dataJSON = JSON.parse(readFileSync('data.json', 'utf-8'));
   return dataJSON;
 };
-const notes = [];
+const notesArr = [];
+const notesObj = readData();
+for (const note in notesObj.notes) {
+  notesArr.push(notesObj.notes[note]);
+}
 const app = express();
 
-const badReq = {
-  error: 'id must be positive integer'
-};
+const note = notesArr[0].id; // delete later
+console.log(note);
 
 app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
-  const notesObj = readData();
-  for (const note in notesObj.notes) {
-    notes.push(notesObj.notes[note]);
-  }
-  res.status(200).json(notes);
+  res.status(200).json(notesArr);
 });
 
 app.get('/api/notes/:id', (req, res) => {
-  console.log(req.params.id);
-  if (req.params.id < 0) {
-    res.status(400).json(badReq);
-  } else if (req.params.id > 0) {
+  const reqToNum = Number(req.params.id);
+  if (reqToNum < 0) {
+    res.status(400).json({ error: 'id must be positive integer' });
+  } else if (reqToNum > 0) {
     let targetId;
-    for (const note in notes) {
-      if (note === req.params.id) {
-        targetId = note.id;
-        req.status(200).json(targetId);
-        console.log(targetId);
+    for (const note in notesArr) {
+      targetId = notesArr[note];
+      if (targetId.id === reqToNum) {
+        res.status(200).json(targetId);
       } else {
         // req.status(404).json()
       }
