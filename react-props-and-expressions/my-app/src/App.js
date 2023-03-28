9b0ad1a9b6373b3af82ac2da820cfde60326d258
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-28 17:37:05 +0000

    in progress

---
+++ react-props-and-expressions/my-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomButton from './custombutton';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          <CustomButton text="I" color="red" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-props-and-expressions/my-app/src/custombutton.js
@@ -0,0 +1,8 @@
+export default function CustomButton (text, color) {
+
+  return (
+    <button>
+      {text}
+    </button>
+  )
+}
