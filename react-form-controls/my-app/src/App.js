1ba76f3477151e91c32bbf689963a5a979a706e4
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-29 23:03:53 +0000

    react-form-control in progress

---
+++ react-form-controls/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import RegistrationFormUncontrolled from './RegistrationFormUncontrolled';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RegistrationFormUncontrolled />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-form-controls/my-app/src/RegistrationFormUncontrolled.js
@@ -0,0 +1,22 @@
+// export default function RegistrationFormUncontrolled() {
+
+//   function handleSubmit() {
+//     event.preventDefault();
+//     console.log()
+//   }
+//   return (
+//     <form>
+//       <label>
+//         Username
+//         <input></input>
+//       </label>
+//       <label>
+//         Password
+//         <input></input>
+//       </label>
+//       <label>
+//         <button>Sign Up</button>
+//       </label>
+//     </form>
+//   )
+// }
