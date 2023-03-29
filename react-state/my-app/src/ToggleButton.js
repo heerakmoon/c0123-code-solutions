dfbc2a2d947b03a9ea6c138c5b8a204df5ebad4e
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-29 02:43:27 +0000

    react-state in progress

---
+++ react-state/my-app/src/ToggleButton.js
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+
+// function ToggleButton({text, color}) {
+//   const [isClicked, setIsClicked] = useState(false);
+
+//   function handleClick() {
+//     setIsClicked(isClicked );
+//   }
+
+//   return (
+//     <button></button>
+//   )
+// }
