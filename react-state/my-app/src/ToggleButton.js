4e11528765cdff8f8b5a70ecf69a06f24a756561
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-29 16:08:44 +0000

    Added console logs and change to duplicate code

--- react-state/my-app/src/ToggleButton.js
+++ react-state/my-app/src/ToggleButton.js
@@ -2,15 +2,17 @@ import { useState } from 'react';
 
 export default function ToggleButton({text, color, onClick}) {
   const [isClicked, setIsClicked] = useState(false);
+  console.log('Value returned by useState:', isClicked)
 
   function handleClick() {
-    console.log('Button clicked:', isClicked);
+    console.log('Value before calling setter:', isClicked);
     setIsClicked(!isClicked);
+    console.log('Value after calling setter:', isClicked);
   }
-
-  if (isClicked) {
+  if (!handleClick) {
     return <button style={{ backgroundColor: color }} onClick={handleClick}>{text}</button>
   } else {
     return <button style={{ backgroundColor: 'white' }} onClick={handleClick}>{text}</button>
   }
+
 }
