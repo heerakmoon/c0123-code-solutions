c2f4441044afbc0e7c1a83ae28fed8ca90aaafe6
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-29 23:43:01 +0000

    react-rendering-lists in progress

---
+++ react-rendering-lists/my-app/src/PokemonList.js
@@ -0,0 +1,8 @@
+export default function PokemonList() {
+  // const pokemonList = pokedex.map((pokemon) => <li key={pokemon.number}> pokemon.name </li>)
+  return (
+    <ul>
+      pokemonList
+    </ul>
+  )
+}
