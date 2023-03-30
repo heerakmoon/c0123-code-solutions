c152a9f102dcaa751fd7a60485efd10c3ce2eba9
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-30 18:44:50 +0000

    validated-input in progress

---
+++ react-validated-input-component/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ValidateInput from './ValidateInput';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ValidateInput />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-validated-input-component/my-app/src/ValidateInput.js
@@ -0,0 +1,26 @@
+// import { useState } from 'react';
+
+// export default function ValidateInput() {
+//   const [input, setInput] = useState(undefined);
+
+//   function handleChange() {
+//     setInput(input)
+//   }
+
+//   return (
+//     <p>
+//       <div>
+//         <label>
+//           Password
+//           <input
+//           onChange={handleChange}
+//           checked
+//           value={}>
+//           </input>
+//           <i className="bi bi-check"></i>
+//         </label>
+//       </div>
+//       <p>a password is required</p>
+//     </p>
+//   )
+// }
