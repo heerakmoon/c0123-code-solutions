0b4e899ccb90a76d5d1bcfc24f4730aa0e16309a
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-31 18:04:45 +0000

    AppDrawer start files

---
+++ react-app-drawer-component/my-app/src/App.js
@@ -0,0 +1,14 @@
+import AppDrawer from './AppDrawer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppDrawer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app-drawer-component/my-app/src/AppDrawer.js
@@ -0,0 +1,16 @@
+
+
+export default function AppDrawer () {
+
+  return (
+    <div>
+      <i></i>
+      <div>
+        <h4>Menuo</h4>
+        <h5>About</h5>
+        <h5>Get Started</h5>
+        <h5>Sign In</h5>
+      </div>
+    </div>
+  )
+}
