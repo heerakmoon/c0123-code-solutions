23ebfec79f493ab12bdb67122cec7f7df0db786d
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-31 20:50:16 +0000

    changed divs into label and input. Added ToggleSwitch css file

--- react-toggle-switch-component/my-app/src/ToggleSwitch.js
+++ react-toggle-switch-component/my-app/src/ToggleSwitch.js
@@ -1,16 +1,18 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// export default function ToggleSwitch ({toggleColor}) {
-//   const [isFlipped, setIsFlipped] = useState(false);
+export default function ToggleSwitch ({toggleColor}) {
+  const [isFlipped, setIsFlipped] = useState(false);
 
-//   function handleClick () {
-//     setIsFlipped(!setIsFlipped);
-//   }
+  function handleClick () {
+    setIsFlipped(!setIsFlipped);
+  }
 
-//   const switchColor = isFlipped ? 'rgb(106, 168, 79)' : 'rgb(226, 226, 226)'
-//   return (
-//     <div onClick={handleClick} stlye={ backgroundColor: switchColor}>
-//       <div onClick={handleClick} style={ backgroundColor: color, position: position}></div>
-//     </div>
-//   )
-// }
+  const switchColor = isFlipped ? 'rgb(106, 168, 79)' : 'rgb(226, 226, 226)'
+  return (
+    <label className="switch" onClick={handleClick} style={{ backgroundColor: switchColor }}>
+      <input type="checkbox" onClick={handleClick}>
+      <span className="slider round"></span>
+      </input>
+    </label>
+  )
+}
