112146ed8735ab4214edb40324125db5cd616e56
Heerak Moon <hmoon3@student.mtsac.edu>
2023-03-31 23:50:28 +0000

    Completed react-validated-input-component exercise

--- react-validated-input-component/my-app/src/ValidateInput.js
+++ react-validated-input-component/my-app/src/ValidateInput.js
@@ -1,26 +1,39 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
-// export default function ValidateInput() {
-//   const [input, setInput] = useState(undefined);
+export default function ValidateInput() {
+  const [input, setInput] = useState('');
 
-//   function handleChange() {
-//     setInput(input)
-//   }
+  function handleOnChange(e) {
+    setInput(e.target.value)
+  }
 
-//   return (
-//     <p>
-//       <div>
-//         <label>
-//           Password
-//           <input
-//           onChange={handleChange}
-//           checked
-//           value={}>
-//           </input>
-//           <i className="bi bi-check"></i>
-//         </label>
-//       </div>
-//       <p>a password is required</p>
-//     </p>
-//   )
-// }
+  const check = input.length < 8;
+  const icon = check ? 'bi bi-x' : 'bi bi-check';
+  const color = check ? 'red' : 'green';
+  let text;
+
+  if (input.length === 0) {
+    text = 'A password is required';
+  } else if (input.length < 8) {
+    text = 'Your password is too short';
+  }
+
+  return (
+  <div>
+    <div>
+      <label>
+        <div>
+        Password
+        </div>
+        <input
+        onChange={handleOnChange}
+        checked
+        value={input}>
+        </input>
+        <i className={icon} style={{ color, fontSize: '35px' }}></i>
+      </label>
+    </div>
+    <p style={{ color }}>{text}</p>
+  </div>
+  )
+}
