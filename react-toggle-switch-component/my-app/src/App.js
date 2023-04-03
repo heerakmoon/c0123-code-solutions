8dc139723d453840fc423359b9621e6f5a5914d7
Heerak Moon <hmoon3@student.mtsac.edu>
2023-04-03 01:19:59 +0000

    Completed react-toggle-switch-component exercise

--- react-toggle-switch-component/my-app/src/App.js
+++ react-toggle-switch-component/my-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ToggleSwitch color='white' />
+        <ToggleSwitch />
       </header>
     </div>
   );
--- react-toggle-switch-component/my-app/src/ToggleSwitch.js
+++ react-toggle-switch-component/my-app/src/ToggleSwitch.js
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import "./ToggleSwitch.css"
 
-export default function ToggleSwitch ({toggleColor}) {
+export default function ToggleSwitch () {
   const [isFlipped, setIsFlipped] = useState(false);
 
   function handleClick () {
-    setIsFlipped(!setIsFlipped);
+    setIsFlipped(!isFlipped);
   }
 
-  const switchColor = isFlipped ? 'rgb(106, 168, 79)' : 'rgb(226, 226, 226)'
+  const flip = isFlipped ? 'on' : 'off';
+  const switchBackground = isFlipped ? 'rgb(65, 106, 186)' : 'rgb(175, 202, 255)'
+  const switchSliderColor = isFlipped ? 'rgb(175, 202, 255)' : 'rgb(65, 106, 186)';
+
   return (
-    <label className="switch" onClick={handleClick} style={{ backgroundColor: switchColor }}>
-      <input type="checkbox" onClick={handleClick}>
-      <span className="slider round"></span>
-      </input>
-    </label>
+    <div>
+      <div className="switch" onClick={handleClick} style={{ backgroundColor: switchBackground }}>
+        <span className={`slider ${flip}`} style={{ backgroundColor: switchSliderColor}}></span>
+      </div>
+        <p>{flip}</p>
+    </div>
   )
 }
