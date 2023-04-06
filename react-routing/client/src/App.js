f77aba3054d4dee972b0c01658804aa98dcaa2cc
Heerak Moon <hmoon3@student.mtsac.edu>
2023-04-06 19:16:53 +0000

    wrapped App component in a BrowserRouter

---
+++ react-routing/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import { Routes, Route } from  'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       {/* <Routes /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
