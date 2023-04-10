0af159aa258527f32fcc76d79113a37edef55371
Heerak Moon <hmoon3@student.mtsac.edu>
2023-04-10 04:50:14 +0000

    Can add to todo and check off

--- fetch-in-react-practice/client/src/components/Todos.js
+++ fetch-in-react-practice/client/src/components/Todos.js
@@ -11,12 +11,6 @@ export default function Todos() {
   const [error, setError] = useState();
 
   useEffect(() => {
-    /* Use fetch to send a GET request to `/api/todos`.
-     * Once the response JSON is received and parsed,
-     * update state with the received todos.
-     * Handle errors correctly and set `error` to any error or error status code.
-     * Set `isLoading` to false when all network activity is finished.
-     */
     async function getTodos () {
       try {
         const res = await fetch(url('/api/todos'));
@@ -35,19 +29,6 @@ export default function Todos() {
   }, []);
 
   async function addTodo(newTodo) {
-    /* Use fetch to send a POST request to `/api/todos`.
-     * Once the response JSON is received and parsed,
-     *   - set the Todos to a new array with the added Todo concatenated
-     *   - handle errors appropriate (as mentioned above)
-     *
-     * DO NOT MUTATE the original state array, nor any objects within it.
-     *
-     * TIP: When calling fetch, be sure to SERIALIZE the todo object in the body
-     * with JSON.stringify() and specify the "Content-Type" header as "application/json"
-     * TIP: The "(prev) =>" version of the state setter function might be convenient
-     * TIP: Use Array.prototype.concat to create a new array containing the contents
-     * of the old array, plus the object returned by the server.
-     */
     const req = {
       method: 'POST',
       headers: {
@@ -68,29 +49,24 @@ export default function Todos() {
     }
   }
 
-  function toggleCompleted(todoId) {
-    /* Find the index of the todo with the matching todoId in the state array.
-     * Get its "isCompleted" status.
-     * Make a new object containing ONE PROPERTY: the opposite "isCompleted" status.
-     * Use fetch to send a PATCH request to `/api/todos/${todoId}`
-     * Once the response JSON is received and parsed,
-     *   - create a shallow copy of the todos array from state
-     *   - replace the old todo with the todo received from the server
-     *   - handle errors appropriate (as mentioned above)
-     *
-     * NOTE: "toggle" means to flip back and forth, so clicking a todo
-     * in the list repeatedly should "toggle" its isCompleted status back and forth.
-     *
-     * DO NOT try to calculate the index of the todo by subtracting 1 from the id.
-     *
-     * DO NOT MUTATE the original state array, nor any objects within it.
-     *
-     * TIP: `Array.map` is a convenient way to make a copy of an array -- compare
-     * the Todo IDs and map the existing Todo if it is not the one that was
-     * just updated, and map the updated array if it is.
-     * TIP: When calling fetch, be sure to SERIALIZE the updates in the body with JSON.stringify()
-     * And specify the "Content-Type" header as "application/json"
-     */
+  async function toggleCompleted(todoId) {
+    const todoIndex = todos.find((todo) => todo.todoId === todoId);
+    const req = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ isCompleted: !todoIndex.isCompleted })
+    };
+    try {
+      const res = await fetch(url(`/api/todos/${todoId}`), req);
+      if (!res.ok) {
+        throw new Error(`Error: status ${res.status}`)
+      }
+      const updated = await res.json();
+      const allTodos = todos.map((original) => original.todoId === updated.todoId ? updated : original);
+      setTodos(allTodos);
+    } catch (error) {
+      setError('Error', error);
+    }
   }
 
   if (isLoading) {
