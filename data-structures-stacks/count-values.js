5db17ca05be1349fdc8d49190a52c28c8c39731d
Heerak Moon <hmoon3@student.mtsac.edu>
2023-04-13 18:11:19 +0000

    Completed data-structures-stacks exercise

--- data-structures-stacks/count-values.js
+++ data-structures-stacks/count-values.js
@@ -1,9 +1,8 @@
 export default function countValues(stack) {
   let countStack = 0;
-  for (let i = 0; i < stack.length; i++) {
-    if (stack.pop() !== undefined) {
-      countStack++;
-    }
+  for (let i = 0; stack.peek() !== undefined; i++) {
+    stack.pop();
+    countStack++;
   }
   return countStack;
 }
--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -1,2 +1,10 @@
 export default function maxValue(stack) {
+  let maxVal = -Infinity;
+  if (stack.peek() !== undefined) {
+    for (let i = 0; stack.peek() !== undefined; i++) {
+      const poppedVal = stack.pop();
+      maxVal = poppedVal > maxVal ? poppedVal : maxVal;
+    }
+  }
+  return maxVal;
 }
