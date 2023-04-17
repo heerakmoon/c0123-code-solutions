17f94f4d49f0b237c6f4e79e99f9845154593fd5
Heerak Moon <hmoon3@student.mtsac.edu>
2023-04-17 18:26:33 +0000

    unit-tests in progress

---
+++ unit-tests-jest/numbers.test.js
@@ -0,0 +1,21 @@
+import { evenNumbers, toDollars, divideBy, multiplyBy } from './numbers';
+
+describe(evenNumbers(), () => {
+
+  it('');
+});
+
+describe(toDollars(), () => {
+
+  it();
+});
+
+describe(divideBy(), () => {
+
+  it();
+});
+
+describe(multiplyBy(), () => {
+
+  it();
+});
