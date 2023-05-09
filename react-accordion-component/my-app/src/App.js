edbc038003668c4276394784c51db83e18344530
Heerak Moon <hmoon3@student.mtsac.edu>
2023-05-09 03:35:51 +0000

    Defined topics at top level and imported Accordion

--- react-accordion-component/my-app/src/App.js
+++ react-accordion-component/my-app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Accordion from './Accordion';
 
 function App() {
+  // const topics = [
+  //   {
+  //     topic: 'Hypertext Markup Language',
+  //     details: 'Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript, it forms a triad of cornerstone technologies for the World Wide Web.',
+  //   },
+  //   {
+  //     topic: 'Cascading Style Sheets',
+  //     details: 'Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML. CSS is a cornerstone technology of the World Wide Web alongside HTML and JavaScript.',
+  //   },
+  //   {
+  //     topic: 'JavaScript',
+  //     details: 'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.',
+  //   }
+  // ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Accordion />
     </div>
   );
 }
